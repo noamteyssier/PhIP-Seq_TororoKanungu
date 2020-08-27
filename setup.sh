3ac1319ce4de14c8bd92e23c81926ce2af5ef07e
noamteyssier <nb.teyssier@gmail.com>
2020-08-27 08:16:58 -0700

    update to setup to update project paths

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ git submodule update --init --recursive
 ## Unzip example files
 gunzip -v data/raw_counts/*.gz
 gunzip -v data/reference_sequences/*.gz
+
+## Update git path in PhageLoader preprocess pipeline
+pl_path=$(realpath .)/src/PhageLoader
+sed -i "s|git_path=.*|git_path=${pl_path}|" ${pl_path}/pipeline/preprocess.sh
